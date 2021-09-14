07bf2f1da930f22e65ff7933a6edde920f98c25b
Xiaotw59 <87548329+Xiaotw59@users.noreply.github.com>
2021-09-14 19:36:04 +0200

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,25 @@
+library(data.table)
+setwd("C:/Users/wxt19/Rfiles/ExData_Plotting1")
+fileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl,destfile="data.zip")
+data<-unzip("data.zip", exdir = getwd())
+View(data)
+data <- read.table("household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".", na.strings = "?")
+str(data)
+head(data)
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+str(subsetdata)
+globalactivepower <- as.numeric(subsetdata$Global_active_power)
+globalreactivepower <- as.numeric(subsetdata$Global_reactive_power)
+voltage <- as.numeric(subsetdata$Voltage)
+subMetering1 <- as.numeric(subsetdata$Sub_metering_1)
+subMetering2 <- as.numeric(subsetdata$Sub_metering_2)
+subMetering3 <- as.numeric(subsetdata$Sub_metering_3)
+
+##plot3
+png(file="plot3.png")
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
